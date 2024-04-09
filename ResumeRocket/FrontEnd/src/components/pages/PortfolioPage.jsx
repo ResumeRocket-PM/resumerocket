3090f6b575201268b6c78d13a2d656050218d87b
AUSTIN GODFREY <17agodfrey@gmail.com>
2024-04-08 21:33:18 -0600

    just took out something from portfolio. pushing so i can pull

--- ResumeRocket/FrontEnd/src/components/pages/PortfolioPage.jsx
+++ ResumeRocket/FrontEnd/src/components/pages/PortfolioPage.jsx
@@ -1,17 +1,28 @@
 import "../../styles/PortfolioPage.css";
 import LeftBarPortfolio from "../LeftBarPortfolio.jsx";
-import { Card, CardContent} from '@mui/material/';
+import { Card, CardContent, Button} from '@mui/material/';
+
+function leftMenu() {
+    return (
+        <div id="portfolio-left_menu_buttons_container">
+            <Button class="portfolio-left_button"/>
+            <Button />
+            <Button />
+            <Button />
+        </div>
+    )
+}
 
 export default function PortfolioPage() {
     return (
         <div id='PortfolioPage_content'>
             <div id='portfolio_left_menu_section'>
-                <LeftBarPortfolio/>
+                <LeftBarPortfolio />
             </div>
             <div id='portfolio_section'>
                 <Card sx={{height:'80vh', width:'60vh'}}>
                     <CardContent>
-                        portfolio goes here idk
+                        portfolio goes here idk.
                     </CardContent>
                 </Card>
             </div>
