b7cfa6f44560d9bd7eeef4ee1348ce2fdfe1811f
tyler <tylerjdebruin@gmail.com>
2024-04-18 01:31:17 -0600

    CreateAcc/Login Page

---
+++ ResumeRocket/FrontEnd/src/components/login-form/LoginForm.jsx
@@ -0,0 +1,231 @@
+import React, { useState } from 'react';
+import './LoginForm.css'; // make sure to create a corresponding CSS file
+import { useSpring, animated } from 'react-spring';
+
+import {AccountCircle, Lock, VisibilityOff, Visibility} from '@mui/icons-material';
+import {Card, CardContent, Typography, TextField, Menu, MenuItem, Button, InputAdornment, IconButton} from '@mui/material/';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+
+  const [values, setValues] = useState({
+    password: '',
+    confirmPassowrd: '',
+    showPassword: false,
+  });
+
+  const handleMouseDownPassword = () => {
+    setValues({ ...values, showPassword: true });
+  };
+
+  const handleMouseUpPassword = () => {
+    setValues({ ...values, showPassword: false });
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+
+  const [showLogin, setShowLogin] = useState(false);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+
+  const handleCreateAccount = async (event) => {
+    console.log("create"); 
+    setShowLogin(false); 
+    setShowCreateAccount(true); 
+}
+  
+  const handleLogin = async (event) => {
+    console.log("log in"); 
+    setShowLogin(true); 
+    setShowCreateAccount(false); 
+};
+
+  const formSpring = useSpring({
+    opacity: showCreateAccount || showLogin  ? 1 : 0,
+    transform: (showCreateAccount || showLogin) ? 'translateY(0)' : 'translateY(-50px)',
+  });
+
+
+  const [loginButtonY, setLoginButtonY] = useState(0);
+  const [createButtonY, setCreateButtonY] = useState(0);
+
+  const loginButtonSpring = useSpring({
+    to: { transform: `translateY(${loginButtonY}px)` },
+    from: { transform: 'translateY(0px)' },
+    marginBottom: 50
+  });
+
+  const createButtonSpring = useSpring({
+    to: { transform: `translateY(${createButtonY}px)` },
+    from: { transform: 'translateY(0px)' },
+    marginBottom: showLogin ? 20 : 0,
+  });
+
+  const swapButtons = () => {
+    setLoginButtonY(loginButtonY === 0 ? 100 : -20); // These values depend on your button height + desired spacing
+    setCreateButtonY(createButtonY === 0 ? -120 : 0);
+  };
+
+  return (
+    <div>
+        <div className="login-form-container">
+
+                <div className="login-form" > <animated.div style={formSpring}>
+                {showCreateAccount && (<>
+
+                    <h3> 
+                    Create Account
+                    </h3>
+
+                    <div className="form-group">
+                        <TextField label="EmailAddress"
+                        fullWidth
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <AccountCircle/>
+                              </InputAdornment>
+                            ),
+                          }}
+                         id="username" 
+                        placeholder="austin@resumerocket.com"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        variant="standard"/>
+                    </div>
+
+                    <div className="form-group">
+                    <TextField
+                        fullWidth
+                        id="password"
+                        variant="standard"
+                        placeholder="password"
+                        required
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                  <Lock/>
+                                </InputAdornment>
+                              ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                    onMouseLeave={handleMouseUpPassword}
+                                    >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                          }}
+                    />
+                    </div>
+
+                    <div className="form-group">
+                    <TextField
+                        fullWidth
+                        id="confirmPassowrd"
+                        variant="standard"
+                        required
+                        placeholder="confirm password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.confirmPassowrd}
+                        onChange={handleChange('confirmPassowrd')}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                  <Lock/>
+                                </InputAdornment>
+                              ),
+                          }}
+                    />
+                    </div>
+
+
+                </>)}
+                {showLogin && (<>
+                    <h3> 
+                    Login
+                    </h3>
+
+                    <div className="form-group">
+                        <TextField label="EmailAddress"
+                        fullWidth
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <AccountCircle/>
+                              </InputAdornment>
+                            ),
+                          }}
+                         id="username" 
+                        placeholder="austin@resumerocket.com"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        variant="standard"/>
+                    </div>
+
+                    <div className="form-group">
+                    <TextField
+                        fullWidth
+                        id="password"
+                        variant="standard"
+                        placeholder="password"
+                        required
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                  <Lock/>
+                                </InputAdornment>
+                              ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                    onMouseLeave={handleMouseUpPassword}
+                                    >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                          }}
+                    />
+                    </div>
+
+
+                </>)}
+                </animated.div>
+
+                <div className="form-actions">
+                    <animated.div style={loginButtonSpring}>
+                        <Button className="button loginButton" onClick={handleLogin}>Login</Button>
+                    </animated.div>
+
+                    <div className="dividerContainer">
+                            <hr className="divider" />
+                            <span className="dividerText">or</span>
+                            <hr className="divider" />
+                        </div>
+
+                    <animated.div style={createButtonSpring}>
+                        <Button className="button createButton" onClick={handleCreateAccount}>Create Account</Button>
+                    </animated.div>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+);};
+
+export default LoginForm;
+
---
+++ ResumeRocket/FrontEnd/src/components/pages/LoginPage.jsx
@@ -0,0 +1,37 @@
+import '../../styles/LoginPage.css'; // make sure to create a corresponding CSS file
+import resumeRocket from '../../assets/resumeRocket.png'
+import LoginForm from '../login-form/LoginForm';
+
+const LoginPage = () => {
+
+  return (
+    <div>
+        {/* <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-k6RqeWecC7o1e4e1KjSiXmV9MWwIKK9hv5u6zwWfHK5Dv3CD5jg5T0Rb2hqyf6xs"
+        crossorigin="anonymous"
+        /> */}
+
+
+
+        <div className="login-page">
+            <div className="content-container">
+                <div className="image-container">
+                <img src={resumeRocket} alt="Resume Rocket" />
+                </div>
+
+                <div className="form-container">
+                    
+                    <h2> 
+                    Resume Rocket
+                    </h2>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    </div>
+);};
+
+export default LoginPage;
+
