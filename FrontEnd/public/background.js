155492d32a9fef9f6b6c03f1f5c1ea28c0fd02ba
Yinghua Yin <u1297837@umail.utah.edu>
2024-10-11 17:25:35 -0600

    add the extension file, I have only provided the view part (popup.html) I am stucked on the authentication in popup.js

---
+++ FrontEnd/public/background.js
@@ -0,0 +1,8 @@
+﻿chrome.action.onClicked.addListener(tab => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            alert("this is a test in ResumeRocket Extension, not sure what function that I should implement for now");
+        }
+    });
+});
