4901b91131a61b7901d68718c358195fb654796e
AUSTIN GODFREY <17agodfrey@gmail.com>
2025-10-20 11:56:43 -0600

    auto-update v2: test 5

--- FrontEnd/src/components/pages/AccountPage/AccountPage.jsx
+++ FrontEnd/src/components/pages/AccountPage/AccountPage.jsx
@@ -306,7 +306,7 @@ const AccountPage = () => {
                         </div>
 
                         <AccountSectionCard 
-                            title='Experience is watermelon' 
+                            title='Experience is apples' 
                             buttonType={'add'}
                             onButtonClick={() => setDialogOpen('Experience')}
                         >
